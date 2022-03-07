c9abd753006d5322c0812d9ca8d1a98c25219e78
Angelo <ramuzuconcepcion@gmail.com>
2022-03-07 17:24:10 +0800

    Fix footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -33,20 +33,20 @@ function Footer() {
         <Foot>
             <Links>
                 {
-                    isAdvanced ? <span>Check out the <a href={`/?advanced=false${config}`}>standard calculator</a>. </span>
-                        : <span>Check out the <a href={`/?advanced=true${config}`}>advanced calculator</a>. </span>
+                    isAdvanced ? <span>Check out the <a href={`?advanced=false${config}`}>standard calculator</a>. </span>
+                        : <span>Check out the <a href={`?advanced=true${config}`}>advanced calculator</a>. </span>
                 }
                 <div style={{ marginTop: 5 }}>
                     Calculator is available in&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=usd&unit=imperial`}>USD</a>,&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=gbp&unit=imperial`}>GBP</a>,&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=eur&unit=metric`}>EUR</a>&nbsp;and&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=php&unit=metric`}>PHP</a>.
+                    <a href={`?advanced=${isAdvanced}&currency=usd&unit=imperial`}>USD</a>,&nbsp;
+                    <a href={`?advanced=${isAdvanced}&currency=gbp&unit=imperial`}>GBP</a>,&nbsp;
+                    <a href={`?advanced=${isAdvanced}&currency=eur&unit=metric`}>EUR</a>&nbsp;and&nbsp;
+                    <a href={`?advanced=${isAdvanced}&currency=php&unit=metric`}>PHP</a>.
                 </div>
                 <div style={{ marginTop: 5 }}>
                     Change measurement system:&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=${_currencyParam}&unit=metric`}>Metric</a>&nbsp;or&nbsp;
-                    <a href={`/?advanced=${isAdvanced}&currency=${_currencyParam}&unit=imperial`}>Imperial</a>&nbsp;
+                    <a href={`?advanced=${isAdvanced}&currency=${_currencyParam}&unit=metric`}>Metric</a>&nbsp;or&nbsp;
+                    <a href={`?advanced=${isAdvanced}&currency=${_currencyParam}&unit=imperial`}>Imperial</a>&nbsp;
                 </div>
             </Links>
             <Author>©️ Ramce Concepcion</Author>
